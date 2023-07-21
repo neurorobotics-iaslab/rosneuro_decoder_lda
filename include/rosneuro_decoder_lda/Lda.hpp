0ea0b95720c1a5b103fa0bed1f81467d34c91db7
PaoloFori <forin.paolo98@gmail.com>
2023-07-21 16:21:07 +0200

    build up the pkg

---
+++ include/rosneuro_decoder_lda/Lda.hpp
@@ -0,0 +1,14 @@
+#ifndef ROSNEURO_DECODER_LDA_HPP
+#define ROSNEURO_DECODER_LDA_HPP
+
+#include "rosneuro_decoder/GenericDecoder.h"
+
+namespace rosneuro{
+    namespace decoder{
+        class Lda : public GenericDecoder{
+
+        };
+    }
+}
+
+#endif
---
+++ src/Lda.cpp
@@ -0,0 +1,12 @@
+#ifndef ROSNEURO_DECODER_LDA_CPP
+#define ROSNEURO_DECODER_LDA_CPP
+
+#include "rosneuro_decoder_lda/Lda.hpp"
+
+namespace rosneuro{
+    namespace decoder{
+
+    }
+}
+
+#endif
